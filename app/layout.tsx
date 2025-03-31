28d78c6df33e245b621458dc902cc45dc18aa864
shielamae02 <shiela.mlepon@gmail.com>
2025-03-31 17:15:06 +0800

    refactor: replace Geist font with Poppins in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
+  variable: "--font-poppins",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
@@ -23,11 +19,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
-      >
-        <main className="min-h-screen w-screen">{children}</main>
+    <html lang='en' className={poppins.variable}>
+      <body className={`antialiased overflow-x-hidden`}>
+        <main className='min-h-screen w-screen'>{children}</main>
       </body>
     </html>
   );
