8b556cdf66850c8eddc826c040e93ef7755fa81b
shielamae02 <shiela.mlepon@gmail.com>
2025-03-31 17:48:30 +0800

    chore: enhance dashboard page to use Zustand for session ID management

--- app/(routes)/dashboard/[id]/page.tsx
+++ app/(routes)/dashboard/[id]/page.tsx
@@ -1,13 +1,23 @@
-interface PageProps {
-  params: { id: string };
-}
+"use client";
 
-export default function Page({ params }: PageProps) {
-  const { id } = params;
+import { use } from "react";
+import { useEffect } from "react";
+import { useSessionStore } from "@/store/sessionStore";
+
+export default function Page({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = use(params);
+
+  const sessionId = useSessionStore((state) => state.sessionId);
+  const setSessionId = useSessionStore((state) => state.setSessionId);
+
+  useEffect(() => {
+    setSessionId(id);
+  }, [id, setSessionId]);
 
   return (
     <main>
-      <h1>Dashboard for session id: {id}</h1>
+      <h1>Dashboard for session id from URL: {id}</h1>
+      <h1>Session ID in Zustand: {sessionId}</h1>
     </main>
   );
 }
