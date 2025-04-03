351bf436dd8ad79aa5785ffaea043fce95549708
shielamae02 <shiela.mlepon@gmail.com>
2025-04-04 01:35:51 +0800

    feat: add custom hook for deleting participants with error handling and notifications

---
+++ hooks/participants/useDeleteParticipant.ts
@@ -0,0 +1,38 @@
+import { toast } from "sonner";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+const deleteParticipantRequest = async ({ id }: { id: string }) => {
+  const result = await fetch("/api/participants", {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ id: id }),
+  });
+
+  const response = await result.json();
+
+  if (!result.ok) {
+    throw new Error("Failed to delete participant");
+  }
+
+  return response;
+};
+
+const useDeleteParticipant = () => {
+  const queryClient = useQueryClient();
+
+  const { mutateAsync: deleteParticipant } = useMutation({
+    mutationFn: deleteParticipantRequest,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["participants"] });
+      toast.success("Successfully deleted the participant.");
+    },
+    onError: (err: any) => {
+      toast.error("Failed to delete participant.");
+      console.error(err);
+    },
+  });
+
+  return { deleteParticipant };
+};
+
+export default useDeleteParticipant;
