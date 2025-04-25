7d2feeb858f13fbd5054f6758e2ba30a9e3eb102
shielamae02 <shiela.mlepon@gmail.com>
2025-04-25 16:11:18 +0800

    refactor: remove metadata from Layout component

--- app/(routes)/dashboard/[id]/layout.tsx
+++ app/(routes)/dashboard/[id]/layout.tsx
@@ -1,12 +1,6 @@
 import { Metadata } from "next";
 import DashboardLayout from "./dashboardLayout";
 
-export const metadata: Metadata = {
-  title: "Dashboard | ChipIn",
-  description:
-    "Effortlessly split costs for trips, events, and daily expenses with our easy-to-use web-based tool.",
-};
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <DashboardLayout>{children}</DashboardLayout>;
 }
