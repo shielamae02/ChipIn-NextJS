0a231c1f226c625b366f919cd2f100a945c57e0b
shielamae02 <shiela.mlepon@gmail.com>
2025-04-25 16:11:39 +0800

    refactor: remove metadata from dashboard page

--- app/(routes)/dashboard/[id]/page.tsx
+++ app/(routes)/dashboard/[id]/page.tsx
@@ -1,11 +1,3 @@
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Dashboard | ChipIn",
-  description:
-    "Effortlessly split costs for trips, events, and daily expenses with our easy-to-use web-based tool.",
-};
-
 export default function Page() {
   return <></>;
 }
