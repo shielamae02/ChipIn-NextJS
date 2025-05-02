0bd6c8baa8f5eaef92576eb62aab42126262aed7
shielamae02 <shiela.mlepon@gmail.com>
2025-05-02 16:50:22 +0800

    fix: remove duplicate router push after creating session

--- components/session/CreateSession.tsx
+++ components/session/CreateSession.tsx
@@ -41,6 +41,7 @@ const CreateSession = () => {
 
     try {
       const response = await createSession(data);
+      router.push(`/dashboard/${response.id}/participants`);
       await createParticipant({
         values: {
           name: data.creator,
@@ -48,7 +49,6 @@ const CreateSession = () => {
         },
         session_id: response.id,
       });
-      router.push(`/dashboard/${response.id}/participants`);
       reset();
     } catch (err) {
       console.error("Error creating session:", err);
