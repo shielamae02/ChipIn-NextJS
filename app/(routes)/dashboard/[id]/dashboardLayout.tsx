9ec3b1ff9d68624c25e38876fbc32fd0fe2cf334
shielamae02 <shiela.mlepon@gmail.com>
2025-05-02 15:17:11 +0800

    feat: enhance DashboardLayout with event creation functionality and UI improvements

--- app/(routes)/dashboard/[id]/dashboardLayout.tsx
+++ app/(routes)/dashboard/[id]/dashboardLayout.tsx
@@ -3,11 +3,13 @@
 import clsx from "clsx";
 import Link from "next/link";
 import { useEffect } from "react";
-import { Calendar1 } from "lucide-react";
-import { formatDate } from "@/lib/utils";
+import { Calendar1, Plus } from "lucide-react";
+import { formatDateTime } from "@/lib/utils";
 import { useSessionStore } from "@/store/sessionStore";
 import { DashboardHeader } from "@/components/dashboard/DashboardHeader";
 import { usePathname, useRouter } from "next/navigation";
+import { CreateEventDialog } from "@/components/events/CreateEventDialog";
+import { Button } from "@/components/ui/button";
 
 export default function DashboardLayout({
   children,
@@ -32,10 +34,10 @@ export default function DashboardLayout({
   ];
 
   return (
-    <div className='min-h-screen flex flex-col h-full'>
-      {/* remove <Head> if you now use metadata */}
+    <div className='min-h-screen flex flex-col h-full max-w-6xl mx-auto px-4 sm:px-'>
+      <div></div>
       <DashboardHeader />
-      <main className='container py-6 mx-auto px-4 sm:px-2 flex h-full flex-1 flex-col space-y-5'>
+      <main className='py-6 flex h-full flex-1 flex-col space-y-5'>
         <div className='flex justify-between w-full items-end'>
           <div className='space-y-1'>
             <h1 className='text-2xl font-semibold sm:text-5xl'>
@@ -45,32 +47,43 @@ export default function DashboardLayout({
               {session && `created by ${session.creator}`}
             </div>
           </div>
-          <p className='text-sm sm:text-md text-muted-foreground flex items-center'>
+          <p className='text-xs sm:text-sm text-muted-foreground flex items-center'>
             <Calendar1 className='size-3 mr-1 sm:size-4' />
-            {session?.created_at ? formatDate(session.created_at) : ""}
+            {session?.created_at ? formatDateTime(session.created_at) : ""}
           </p>
         </div>
 
-        <div className='flex gap-2'>
-          {tabs.map((tab) => {
-            const href = `${basePath}/${tab.path}`;
-            const isActive = pathname === href;
+        <div className='flex justify-between gap-1'>
+          <div className=' flex gap-1 sm:gap-2'>
+            {tabs.map((tab) => {
+              const href = `${basePath}/${tab.path}`;
+              const isActive = pathname === href;
 
-            return (
-              <Link key={tab.path} href={href}>
-                <button
-                  className={clsx(
-                    "text-xs sm:text-sm font-medium py-2 px-3.5 w-fit rounded-lg transition",
-                    isActive
-                      ? "bg-zinc-900 text-white"
-                      : "bg-zinc-100 hover:bg-zinc-200"
-                  )}
-                >
-                  {tab.label}
-                </button>
-              </Link>
-            );
-          })}
+              return (
+                <Link key={tab.path} href={href}>
+                  <button
+                    className={clsx(
+                      "text-xs sm:text-sm font-medium py-2 px-3.5 w-fit rounded-lg transition",
+                      isActive
+                        ? "bg-zinc-900 text-white"
+                        : "bg-zinc-100 hover:bg-zinc-200"
+                    )}
+                  >
+                    {tab.label}
+                  </button>
+                </Link>
+              );
+            })}
+          </div>
+
+          {pathname.endsWith("/events") && (
+            <CreateEventDialog>
+              <Button className='text-sm h-8 bg-zinc-800 hover:bg-zinc-900 text-white shadow-lg transition duration-300'>
+                <Plus />
+                <span className='hidden sm:block'>Add Event</span>
+              </Button>
+            </CreateEventDialog>
+          )}
         </div>
 
         <div className='pt-3 w-full h-full flex-1 flex flex-col'>
