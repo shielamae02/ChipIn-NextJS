31b509d0329930f65ddf8430b3776a103c8ce56c
shielamae02 <shiela.mlepon@gmail.com>
2025-05-02 15:18:29 +0800

    feat: replace summary header with SettlementSummary component

--- app/(routes)/dashboard/[id]/summary/page.tsx
+++ app/(routes)/dashboard/[id]/summary/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import SettlementSummary from "@/components/settlement/SettlementSummary";
+import { useEvents, useParticipants } from "@/hooks";
+import { useParams } from "next/navigation";
+
 export default function Page() {
+  const { id } = useParams();
+  const session_id = id as string;
+  const { events } = useEvents(session_id);
+  const { participants } = useParticipants(session_id);
+
   return (
     <section>
-      <h1>Summary</h1>
+      <SettlementSummary participants={participants} events={events} />
     </section>
   );
 }
