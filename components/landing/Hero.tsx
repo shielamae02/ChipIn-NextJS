81d97ac6f42a503c4370d36f0869bc1f75844a85
shielamae02 <shiela.mlepon@gmail.com>
2025-05-02 15:21:46 +0800

    fix: improve layout and spacing in Hero component for better readability

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -8,12 +8,13 @@ const Hero = () => {
     <section className='py-38 md:py-28 bg-gradient-to-b from-background to-muted/50'>
       <div className='container px-4 md:px-6 mx-auto flex space-x-10'>
         <div className='flex flex-col lg:flex-row w-full xl:px-24'>
-          <div className='flex flex-col justify-center space-y-4 w-full'>
-            <div className='space-y-4'>
-              <h1 className='text-5xl font-bold tracking-tight sm:text-5xl xl:text-6xl/none'>
-                Split Expenses Effortlessly with ChipIn
+          <div className='flex flex-col justify-center items-center space-y-6 w-full'>
+            <div className='space-y-8 flex flex-col items-center text-center'>
+              <h1 className='text-5xl font-bold sm:text-5xl xl:text-6xl/none'>
+                Split Expenses Effortlessly <br />
+                with ChipIn
               </h1>
-              <p className='max-w-[600px] text-muted-foreground md:text-xl'>
+              <p className='max-w-3xl text-muted-foreground md:text-xl'>
                 The web-based expense splitter that makes managing shared costs
                 simple for trips, events, or everyday household expenses.
               </p>
@@ -24,12 +25,14 @@ const Hero = () => {
                   Create a Session <ArrowRight className='h-4 w-4' />
                 </Button>
               </Link>
-              <Button size='lg' variant='outline'>
-                Join a Session
-              </Button>
+              <Link href='/join'>
+                <Button size='lg' variant='outline'>
+                  Join a Session
+                </Button>
+              </Link>
             </div>
           </div>
-          <div className='hidden lg:block ml-3'>
+          {/* <div className='hidden lg:block ml-3'>
             <Image
               src='/placeholder.svg?height=550&width=450'
               alt='ChipIn App Screenshot'
@@ -37,7 +40,7 @@ const Hero = () => {
               width={500}
               height={500}
             />
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
