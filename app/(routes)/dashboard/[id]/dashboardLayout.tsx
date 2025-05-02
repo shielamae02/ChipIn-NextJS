eba1420732ecd7de61b30b2ef395fe876aaa4df9
shielamae02 <shiela.mlepon@gmail.com>
2025-05-03 00:16:35 +0800

    feat: integrate useParticipants hook and enhance event button logic

--- app/(routes)/dashboard/[id]/dashboardLayout.tsx
+++ app/(routes)/dashboard/[id]/dashboardLayout.tsx
@@ -11,6 +11,7 @@ import { usePathname, useRouter } from "next/navigation";
 import { CreateEventDialog } from "@/components/events/CreateEventDialog";
 import { Button } from "@/components/ui/button";
 import { FadeIn } from "@/components/shared/FadeIn";
+import { useParticipants } from "@/hooks";
 
 export default function DashboardLayout({
   children,
@@ -20,11 +21,12 @@ export default function DashboardLayout({
   const pathname = usePathname();
   const router = useRouter();
   const { session, hasHydrated } = useSessionStore();
+  const { participants } = useParticipants(session?.id);
 
   useEffect(() => {
     if (!hasHydrated) return;
     if (!session) router.replace("/join");
-  }, [hasHydrated, session]);
+  }, [hasHydrated, session, router]);
 
   const basePath = pathname.split("/").slice(0, 3).join("/");
 
@@ -35,72 +37,76 @@ export default function DashboardLayout({
   ];
 
   return (
-    <div className='min-h-screen flex flex-col h-full max-w-6xl mx-auto px-4 sm:px-'>
-      <div></div>
+    <>
       <DashboardHeader />
-      <main className='py-6 flex h-full flex-1 flex-col space-y-5'>
-        <div className='flex justify-between w-full items-end'>
-          <div className='space-y-1'>
-            <FadeIn duration={100}>
-              <h1 className='text-2xl font-semibold sm:text-5xl'>
-                {session?.name}
-              </h1>
-            </FadeIn>
-            <FadeIn duration={200}>
-              <div className='flex text-muted-foreground text-xs sm:text-sm'>
-                {session && `created by ${session.creator}`}
-              </div>
-            </FadeIn>
-          </div>
-          <FadeIn duration={200}>
-            <p className='text-xs sm:text-sm text-muted-foreground flex items-center'>
-              <Calendar1 className='size-3 mr-1 sm:size-4' />
-              {session?.created_at ? formatDateTime(session.created_at) : ""}
-            </p>
-          </FadeIn>
-        </div>
-
-        <FadeIn duration={300}>
-          <div className='flex justify-between gap-1'>
-            <div className=' flex gap-1 sm:gap-2'>
-              {tabs.map((tab) => {
-                const href = `${basePath}/${tab.path}`;
-                const isActive = pathname === href;
-
-                return (
-                  <Link key={tab.path} href={href}>
-                    <button
-                      className={clsx(
-                        "text-xs sm:text-sm font-medium py-2 px-3.5 w-fit rounded-lg transition",
-                        isActive
-                          ? "bg-zinc-900 text-white"
-                          : "bg-zinc-100 hover:bg-zinc-200"
-                      )}
-                    >
-                      {tab.label}
-                    </button>
-                  </Link>
-                );
-              })}
+      <div className='min-h-screen flex flex-col h-full max-w-6xl mx-auto px-4 sm:px-6'>
+        <main className='py-6 flex h-full flex-1 flex-col space-y-5 '>
+          <div className='flex justify-between w-full items-end'>
+            <div className='space-y-1'>
+              <FadeIn duration={100}>
+                <h1 className='text-2xl font-semibold sm:text-5xl'>
+                  {session?.name}
+                </h1>
+              </FadeIn>
+              <FadeIn duration={200}>
+                <div className='flex text-muted-foreground text-xs sm:text-sm'>
+                  {session && `created by ${session.creator}`}
+                </div>
+              </FadeIn>
             </div>
-
-            {pathname.endsWith("/events") && (
-              <CreateEventDialog>
-                <Button className='text-sm h-8 bg-zinc-800 hover:bg-zinc-900 text-white shadow-lg transition duration-300'>
-                  <Plus />
-                  <span className='hidden sm:block'>Add Event</span>
-                </Button>
-              </CreateEventDialog>
-            )}
+            <FadeIn duration={200}>
+              <p className='text-xs sm:text-sm text-muted-foreground flex items-center'>
+                <Calendar1 className='size-3 mr-1 sm:size-4' />
+                {session?.created_at ? formatDateTime(session.created_at) : ""}
+              </p>
+            </FadeIn>
           </div>
-        </FadeIn>
 
-        <FadeIn duration={400}>
-          <div className='pt-3 w-full h-full flex-1 flex flex-col'>
-            {children}
-          </div>
-        </FadeIn>
-      </main>
-    </div>
+          <FadeIn duration={300}>
+            <div className='flex justify-between gap-1'>
+              <div className=' flex gap-1 sm:gap-2'>
+                {tabs.map((tab) => {
+                  const href = `${basePath}/${tab.path}`;
+                  const isActive = pathname === href;
+
+                  return (
+                    <Link key={tab.path} href={href}>
+                      <button
+                        className={clsx(
+                          "text-xs sm:text-sm font-medium py-2 px-3.5 w-fit rounded-lg transition",
+                          isActive
+                            ? "bg-zinc-900 text-white"
+                            : "bg-zinc-100 hover:bg-zinc-200"
+                        )}
+                      >
+                        {tab.label}
+                      </button>
+                    </Link>
+                  );
+                })}
+              </div>
+
+              {pathname.endsWith("/events") && (
+                <CreateEventDialog>
+                  <Button
+                    disabled={participants.length < 2}
+                    className='text-sm bg-zinc-800 hover:bg-zinc-900 text-white shadow-lg transition duration-300'
+                  >
+                    <Plus />
+                    <span className='hidden sm:block'>Add Event</span>
+                  </Button>
+                </CreateEventDialog>
+              )}
+            </div>
+          </FadeIn>
+
+          <FadeIn duration={400}>
+            <div className='pt-3 w-full h-full flex-1 flex flex-col'>
+              {children}
+            </div>
+          </FadeIn>
+        </main>
+      </div>
+    </>
   );
 }
