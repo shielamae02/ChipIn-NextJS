79749134844d26e16350d9aa44fe55678f3a49d7
shielamae02 <shiela.mlepon@gmail.com>
2025-05-02 15:20:03 +0800

    fix: update ShareSessionDialog to use session object for ID and name

--- components/dashboard/ShareSessionDialog.tsx
+++ components/dashboard/ShareSessionDialog.tsx
@@ -10,19 +10,22 @@ import {
 import { copyToClipboard } from "@/lib/utils";
 import { CreditCard, Link2 } from "lucide-react";
 import { toast } from "sonner";
+import { Session } from "@/types/session";
 
 interface ShareSessionDialogProps {
   children: React.ReactNode;
-  sessionId: string;
+  session: Session;
 }
 
 const ShareSessionDialog: React.FC<ShareSessionDialogProps> = ({
   children,
-  sessionId,
+  session,
 }) => {
+  const url = session ? `${process.env.NEXT_PUBLIC_BASE_URL}/join` : "";
+
   const handleCopy = async () => {
     copyToClipboard(
-      sessionId,
+      session.id,
       "Session ID copied to clipboard!",
       "Failed to copy the Session ID."
     );
@@ -38,7 +41,7 @@ const ShareSessionDialog: React.FC<ShareSessionDialogProps> = ({
               <CreditCard className='h-5 w-5 text-primary' />
             </div>
           </div>
-          <DialogTitle className='text-xl'>SessionName</DialogTitle>
+          <DialogTitle className='text-xl'>{session.name}</DialogTitle>
           <DialogDescription className='text-center text-sm'>
             Want your friends to join this session?
           </DialogDescription>
@@ -46,11 +49,11 @@ const ShareSessionDialog: React.FC<ShareSessionDialogProps> = ({
         <div className='space-y-2 flex flex-col items-center bg-zinc-100 w-full p-2.5 rounded-md'>
           <p className='text-xs text-center'>
             Share the session ID so they can join you at <br />
-            <span className='font-semibold'>chipin-io.vercel.app/join.</span>
+            <span className='font-semibold'>{url}.</span>
           </p>
           <div className='flex flex-col sm:flex-row space-y-2 sm:space-y-0 justify-center items-center space-x-2 w-full'>
             <p className='bg-white p-2.5 text-xs rounded-sm w-full text-center'>
-              {sessionId}
+              {session.id}
             </p>
             <Button onClick={handleCopy} className='w-full sm:w-fit'>
               Copy
