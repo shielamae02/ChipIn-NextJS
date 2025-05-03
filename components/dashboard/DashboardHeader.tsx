15feac92cf5a2500307e8a8d17861a5ec1cd491c
shielamae02 <shiela.mlepon@gmail.com>
2025-05-03 22:29:16 +0800

    refactor: improve layout and formatting in DashboardHeader component

--- components/dashboard/DashboardHeader.tsx
+++ components/dashboard/DashboardHeader.tsx
@@ -11,11 +11,10 @@ const DashboardHeader = () => {
   const session = useSessionStore((state) => state.session);
   const clearSession = useSessionStore((state) => state.clearSession);
 
-
   if (!session) {
     return (
       <header className='sticky top-0 z-10  bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
-        <div className='px-4 sm:px-2 py-10 flex h-16 items-center justify-between'>
+        <div className='py-10 flex h-16 items-center mx-auto px-4 sm:px-6 max-w-6xl'>
           <div className='flex gap-2'>
             <Skeleton className='h-8 w-24' />
             <Skeleton className='h-8 w-36' />
@@ -44,7 +43,7 @@ const DashboardHeader = () => {
         </ConfirmationDialog>
 
         <div className='ml-auto flex gap-2'>
-          <ShareQRCodeDialog  sessionName={session.name}>
+          <ShareQRCodeDialog sessionName={session.name}>
             <Button variant='outline' size='sm'>
               <ScanQrCode className='mr-0 sm:mr-1 size-4' />
               <p className='hidden sm:block'>Show QR Code</p>
