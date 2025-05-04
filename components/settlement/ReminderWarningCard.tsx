2a546a47544c7e62df21ead956be20ef218137da
shielamae02 <shiela.mlepon@gmail.com>
2025-05-05 00:13:10 +0800

    fix: refactor ReminderWarningCard to accept dynamic content and className props

--- components/settlement/ReminderWarningCard.tsx
+++ components/settlement/ReminderWarningCard.tsx
@@ -1,18 +1,22 @@
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { CircleAlert } from "lucide-react";
 
-const ReminderWarningCard = () => {
+interface Props {
+  content: string;
+  className?: string;
+}
+
+const ReminderWarningCard: React.FC<Props> = ({ content, className }) => {
   return (
-    <Alert className='w-fit bg-amber-50 text-amber-800 dark:bg-amber-900/20 dark:text-amber-400 border-amber-200 dark:border-amber-800/30'>
+    <Alert
+      className={`bg-amber-50 w-fit text-amber-800 dark:bg-amber-900/20 dark:text-amber-400 border-amber-200 dark:border-amber-800/30 ${className}`}
+    >
       <AlertTitle className='flex text-sm items-center'>
         <CircleAlert className='size-3 mr-1' />
         Heads up!
       </AlertTitle>
 
-      <AlertDescription className='text-xs flex'>
-        This session lasts one day—don’t forget to download your expenses data
-        before it’s gone!
-      </AlertDescription>
+      <AlertDescription className='text-xs flex'>{content}</AlertDescription>
     </Alert>
   );
 };
