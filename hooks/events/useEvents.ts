4446e88f164f632b1ee683dadeb2c95efb07900d
shielamae02 <shiela.mlepon@gmail.com>
2025-05-05 00:14:18 +0800

    ref: update queryKey structure in useEvents hook

--- hooks/events/useEvents.ts
+++ hooks/events/useEvents.ts
@@ -5,7 +5,7 @@ import { useQuery } from "@tanstack/react-query";
 
 const useEvents = (session_id?: string) => {
   const { data: events = [], isLoading } = useQuery<Event[]>({
-    queryKey: ["events", session_id],
+    queryKey: ["events", { session_id }],
     queryFn: async () => {
       const response = await fetch(`/api/events?session_id=${session_id}`);
       if (!response.ok) {
